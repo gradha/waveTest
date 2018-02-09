acd3b0e698f7da1635c96c96df1bd354436089c1
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2018-02-09 11:05:32 +0100

    Adds empty activity template.

---
+++ vintage/app/src/main/java/es/elhaso/gradha/churfthewave/MainActivity.java
@@ -0,0 +1,15 @@
+package es.elhaso.gradha.churfthewave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity
+    extends AppCompatActivity
+{
+
+    @Override protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
