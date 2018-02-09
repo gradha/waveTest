f13bd60314393d72afd35139476205e00586354e
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2018-02-09 11:30:34 +0100

    Replaces email with username.

--- vintage/app/src/main/java/es/elhaso/gradha/churfthewave/ui/LoginActivity.java
+++ vintage/app/src/main/java/es/elhaso/gradha/churfthewave/ui/LoginActivity.java
@@ -3,39 +3,23 @@ package es.elhaso.gradha.churfthewave.ui;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import es.elhaso.gradha.churfthewave.BuildConfig;
 import es.elhaso.gradha.churfthewave.R;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -47,8 +31,8 @@ public class LoginActivity
     private @Nullable UserLoginTask mAuthTask;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
+    private AutoCompleteTextView mUserEdit;
+    private EditText mPasswordEdit;
     private View mProgressView;
     private View mLoginFormView;
 
@@ -57,10 +41,17 @@ public class LoginActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mEmailView = findViewById(R.id.email);
-        mPasswordView = findViewById(R.id.password);
+        mUserEdit = findViewById(R.id.email);
+        mPasswordEdit = findViewById(R.id.password);
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
 
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        if (BuildConfig.DEBUG) {
+            mUserEdit.setText("mock_user");
+            mPasswordEdit.setText("mock_password");
+        }
+
+        mPasswordEdit.setOnEditorActionListener(new TextView.OnEditorActionListener()
 
         {
             @Override public boolean onEditorAction(TextView textView,
@@ -84,8 +75,6 @@ public class LoginActivity
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
     }
 
     /**
@@ -102,31 +91,31 @@ public class LoginActivity
         */
 
         // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
+        mUserEdit.setError(null);
+        mPasswordEdit.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        String user = mUserEdit.getText().toString();
+        String password = mPasswordEdit.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
+            mPasswordEdit.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordEdit;
             cancel = true;
         }
 
         // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+        if (TextUtils.isEmpty(user)) {
+            mUserEdit.setError(getString(R.string.error_field_required));
+            focusView = mUserEdit;
             cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
+        } else if (!isUserValid(user)) {
+            mUserEdit.setError(getString(R.string.error_invalid_username));
+            focusView = mUserEdit;
             cancel = true;
         }
 
@@ -138,14 +127,14 @@ public class LoginActivity
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
+            mAuthTask = new UserLoginTask(user, password);
             mAuthTask.execute((Void) null);
         }
     }
 
-    private boolean isEmailValid(String email)
+    private boolean isUserValid(String user)
     {
-        return email.contains("@");
+        return user.length() > 0;
     }
 
     private boolean isPasswordValid(String password)
@@ -197,13 +186,12 @@ public class LoginActivity
         extends AsyncTask<Void, Void, Boolean>
     {
 
-        private final String mEmail;
+        private final String mUser;
         private final String mPassword;
 
-        UserLoginTask(String email,
-            String password)
+        UserLoginTask(String user, String password)
         {
-            mEmail = email;
+            mUser = user;
             mPassword = password;
         }
 
@@ -221,7 +209,7 @@ public class LoginActivity
             /*
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
+                if (pieces[0].equals(mUser)) {
                     // Account exists, return true if the password matches.
                     return pieces[1].equals(mPassword);
                 }
@@ -240,8 +228,8 @@ public class LoginActivity
             if (success) {
                 finish();
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+                mPasswordEdit.setError(getString(R.string.error_incorrect_password));
+                mPasswordEdit.requestFocus();
             }
         }
 
