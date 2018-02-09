d7a1d45729fc61863413eedaccb8f6fa35fe0a81
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2018-02-09 11:09:53 +0100

    Removes constraint layout library, creates UI package.

--- vintage/app/src/main/java/es/elhaso/gradha/churfthewave/ui/MainActivity.java
+++ vintage/app/src/main/java/es/elhaso/gradha/churfthewave/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package es.elhaso.gradha.churfthewave;
+package es.elhaso.gradha.churfthewave.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import es.elhaso.gradha.churfthewave.R;
 
 public class MainActivity
     extends AppCompatActivity
